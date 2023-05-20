7eb9491697e7c997a149cbda0e0884d1fd1aa298
José Ambrosio Bastián <51802669+jambdeep@users.noreply.github.com>
2023-05-20 08:23:08 -0600

    Update sw.js

--- page/js/sw.js
+++ page/js/sw.js
@@ -5,9 +5,9 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      'index.html',
-      'js/converter.js',
-      'css/converter.css'
+      './',
+      './js/converter.js',
+      './css/converter.css'
     ]);
   })());
 });
