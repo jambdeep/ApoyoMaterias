95d8738354161da6de8155e6e4eabe716fc8658c
José Ambrosio Bastián <51802669+jambdeep@users.noreply.github.com>
2023-05-20 09:00:43 -0600

    Update sw.js

--- page/js/sw.js
+++ page/js/sw.js
@@ -5,7 +5,8 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '.'
+      '.',
+      './img/icon512.png'
     ]);
   })());
 });
